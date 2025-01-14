b7572e575b117b603853fd0786b88195c7c15800
davisclarke <clarked126@gmail.com>
2025-01-14 01:38:55 -0500

    conda configs

--- install.sh
+++ install.sh
@@ -35,3 +35,11 @@ mkdir $HOME/repos
 cp -r ./sddm/sddm-astronaut-theme/ /usr/share/sddm/themes/ 
 cp ./sddm/default.conf /usr/lib/sddm/sddm.conf.d/
 
+# mkdir -p ~/miniconda3
+#   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+#   bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+#   rm ~/miniconda3/miniconda.sh
+# conda init fish
+# conda config --set changeps1 false 
+# conda config --set auto_activate_base false 
+# confa init fish
