7100f16aacd0bc816c03e646129ee5bec953e872
davisclarke <clarked126@gmail.com>
2025-07-01 01:51:38 -0700

    added light theme to kitty, theme switcher

--- config/sway/dark_theme.sh
+++ config/sway/dark_theme.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Change a few lines to match scheme
 sed -i "s/vim.opt.background = 'light'/vim.opt.background = 'dark'/g" /home/davisc/.config/nvim/init.lua
-sed -i 's/use_dark_theme = false/use_dark_theme = true/g' /home/davisc/.wezterm.lua
+# sed -i 's/use_dark_theme = false/use_dark_theme = true/g' /home/davisc/.wezterm.lua
+kitten themes --cache-age=-1 --reload-in=all Jellybeans 
 sed -i 's/jellybeans_light/jellybeans_dark/g' /home/davisc/.config/sway/config
 sed -i 's/jellybeans-light.rasi/jellybeans-dark.rasi/g' /home/davisc/.config/rofi/config.rasi
 sed -i 's/"mode": "light"/"mode": "dark"/g' /home/davisc/.config/zed/settings.json
--- config/sway/light_theme.sh
+++ config/sway/light_theme.sh
@@ -2,7 +2,8 @@
 
 # Change a few lines to match scheme
 sed -i "s/vim.opt.background = 'dark'/vim.opt.background = 'light'/g" /home/davisc/.config/nvim/init.lua
-sed -i 's/use_dark_theme = true/use_dark_theme = false/g' /home/davisc/.wezterm.lua
+# sed -i 's/use_dark_theme = true/use_dark_theme = false/g' /home/davisc/.wezterm.lua
+kitten themes --cache-age=-1 --reload-in=all Jellybeans-Light
 sed -i 's/jellybeans_dark/jellybeans_light/g' /home/davisc/.config/sway/config
 sed -i 's/jellybeans-dark.rasi/jellybeans-light.rasi/g' /home/davisc/.config/rofi/config.rasi
 sed -i 's/"mode": "dark"/"mode": "light"/g' /home/davisc/.config/zed/settings.json
